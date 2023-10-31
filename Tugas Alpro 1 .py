7e4fd499a065267826265ad9957a429e77797151
GhifariDafa <149139375+GhifariDafa@users.noreply.github.com>
2023-10-31 20:44:11 +0700

    Update Tugas Alpro 1 .py

--- Tugas Alpro 1 .py
+++ Tugas Alpro 1 .py
@@ -22,3 +22,30 @@ elif Pilih_Topping == "Super Supreme Chicken":
     
 else :
     print("Pilihan Menu Anda Tidak Valid")
+
+#Pilih crust
+print('/////Untuk Pilihan Crust/////')
+print('''-Pan Pizza
+-StuffedCrust Cheese
+-StuffedCrust Sausage
+-Cheesy Bites
+-Crown Crust''')
+
+pilih_crust = str(input("pilih  crust yang anda inginkan: "))
+if pilih_crust == 'Pan Pizza' :
+    harga_crust = 0
+    
+elif pilih_crust == 'StuffedCrust Cheese':
+    harga_crust = 11818
+    
+elif pilih_crust == "StuffedCrust Sausage":
+    harga_crust = 9101
+    
+elif pilih_crust == "Cheesy Bites":
+    harga_crust = 13636
+
+elif pilih_crust == 'Crown Crust':
+    harga_crust = 11818
+else:
+    print("Pilihan Crust anda tidak valid")
+
