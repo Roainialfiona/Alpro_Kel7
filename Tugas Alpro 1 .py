a887008c8e8744b5b32bed3b04a0f6f81f5d25fd
135Akbar <149138366+135Akbar@users.noreply.github.com>
2023-11-02 08:50:41 +0700

    Update Tugas Alpro 1 .py

--- Tugas Alpro 1 .py
+++ Tugas Alpro 1 .py
@@ -49,3 +49,33 @@ elif pilih_crust == 'Crown Crust':
 else:
     print("Pilihan Crust anda tidak valid")
 
+#pilih  ukuran pizza
+print('/////Untuk Pilihan Ukuran/////')
+print('''-Personal
+-Reguler
+-Large''')
+
+pilih_ukuran = str(input("pilih ukuran: "))
+if pilih_ukuran == "Personal":
+    harga_ukuran = 0
+elif pilih_ukuran == "Reguler":
+    harga_ukuran = 57273
+elif pilih_ukuran == "Large":
+    harga_ukuran = 89091
+else:
+    print("Ukuran anda tidak ada")
+
+#Tambah Keju
+tambah_keju = str(input("apakah anda ingin menambah extra keju? yes/no: "))
+if tambah_keju == "yes":
+    harga_keju = 16364
+elif tambah_keju == "no":
+    harga_keju = 0
+else :
+    print("jawaban anda tidak valid")
+
+total_harga = harga_toping + harga_crust + harga_ukuran + harga_keju
+
+print("thank you for choosing Pizza Hut deliveries!")
+print("Your final bill is: Rp", total_harga )
+
